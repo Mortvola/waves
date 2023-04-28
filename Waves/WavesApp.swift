b189554d67607e0fa05a36b57bee6fe75488f267
Mortvola <mortvola@gmail.com>
2023-04-28 08:35:32 -0600

    Initial Commit

---
+++ Waves/WavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavesApp.swift
+//  Waves
+//
+//  Created by Richard Shields on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
